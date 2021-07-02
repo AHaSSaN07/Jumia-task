c019b7e74c34c99732ec6e822c3b665d16aa1444
AHaSSaN07 <enga.hassan60@gmail.com>
2021-07-02 18:36:58 +0200

    added support for sql lite db.

--- src/main/java/com/jumia/task/jumiaTask/JumiaTaskApplication.java
+++ src/main/java/com/jumia/task/jumiaTask/JumiaTaskApplication.java
@@ -1,7 +1,14 @@
 package com.jumia.task.jumiaTask;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.jdbc.DataSourceBuilder;
+import org.springframework.context.annotation.Bean;
+import org.springframework.core.env.Environment;
+import org.springframework.jdbc.datasource.DriverManagerDataSource;
+
+import javax.sql.DataSource;
 
 @SpringBootApplication
 public class JumiaTaskApplication {
@@ -9,5 +16,8 @@ public class JumiaTaskApplication {
 	public static void main(String[] args) {
 		SpringApplication.run(JumiaTaskApplication.class, args);
 	}
+
+	@Autowired
+	Environment env;
 
 }
