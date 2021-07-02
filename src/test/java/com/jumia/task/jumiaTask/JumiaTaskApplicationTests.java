ff03f962a3edf0335f7f1a8146ca41681a49e337
AHaSSaN07 <enga.hassan60@gmail.com>
2021-07-02 17:30:32 +0200

    init commit

---
+++ src/test/java/com/jumia/task/jumiaTask/JumiaTaskApplicationTests.java
@@ -0,0 +1,13 @@
+package com.jumia.task.jumiaTask;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class JumiaTaskApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
